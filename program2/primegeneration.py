dbc05bc8171e7f3bab966fa3babc92bd87a9bff3
jtschuler <jtschuler007@yahoo.com>
2020-10-04 01:53:47 -0400

    RSA implementation and documentation complete

--- program2/primegeneration.py
+++ program2/primegeneration.py
@@ -1,15 +1,32 @@
 # Jadon Schuler
-# Generate prime numbers of a certain length using Miller-Rabin
+# Copyright 2020
 
-import numpy as np
+"""This module finds primes, and encryption and decryption exponents for RSA
+
+This module uses the Miller-Rabin method to generate two prime numbers of size
+n_bits bits, which by default is 32. Then, after calculating n = p * q, the
+program prompts the user for a value of e, which must be coprime to phi_n, then
+solves the extended euclidean algorithm to find d. The program then prompts the
+user for the name of a text file, and prints to that text file.
+"""
+
+# Suppress variable name warning (my single-letter
+# variables are meant to be that way)
+# pylint: disable=C0103
+
+# Suppress argument out of order warning (incorrect warning in GCD function)
+# pylint: disable=W1114
+
+# Used for random number generation
 import secrets
-import sys
 
 def main():
+    """Main function for running Miller-Rabin and exponent finding.
+    """
     primes = []
     n_bits = 32
 
-    for i in range(2):
+    for _ in range(2):
         prime = False
         while not prime:
             # Generate a random int of length n_bits bits
@@ -30,7 +47,7 @@ def main():
             print("N = " + str(d) + " * (2 ^ " + str(r) + ")")
 
             prime = True
-            for j in range(128):
+            for _ in range(128):
                 if not millerrabin(n, d, r):
                     prime = False
                     break
@@ -62,7 +79,15 @@ def main():
     file.close()
 
 def find_d_r(n: int):
-    # Need to find d, r such that n - 1 = d * (2 ^ r)
+    """Finds d and r such that n - 1 = d * (2 ^ r)
+
+    Args:
+        n: the number for which to solve the equation
+
+    Returns:
+        d: d in the above equation
+        r: r in the above equation
+    """
     r = 0
     d = n - 1
 
@@ -75,16 +100,29 @@ def find_d_r(n: int):
     return d, r
 
 def millerrabin(n, d, r):
+    """Tests n given d and r for primality
+
+    Miller-Rabin takes advantage of the fact that there are no trivial roots of
+    unity mod n when n is prime.
+
+    Args:
+        n: The prime number candidate
+        d: d such that n - 1 = d * (2 ^ r)
+        r: r such that n - 1 = d * (2 ^ r)
+
+    Returns:
+        False if n is definitely composite, true otherwise
+    """
     a = 0
     while a < 2 or a == n - 1:
         a = secrets.randbelow(n)
     x = modular_exponentiation(a, d, n)
     print(str(a) + " ^ " + str(d) + "\t" + str(x))
 
-    if x == 1 or x == n - 1:
+    if x in (1, n - 1):
         return True
 
-    for i in range(1, r - 1):
+    for _ in range(1, r - 1):
         x = (x * x) % n
         if x == 1:
             return False
@@ -94,28 +132,61 @@ def millerrabin(n, d, r):
 
 # From here on related to finding d from e
 def find_decryption(n):
+    """Finds decryption exponent given n and user-input e
+
+    Choice of e must be coprime to n! Continues to prompt until this is true.
+
+    Args:
+        n: the order of the group formed by p * q, or phi_n
+
+    Returns:
+        e: the valid encryption exponent
+        x % n: the decryption exponent associated with e
+    """
     # ex - ny = 1
     e = int(input("Enter choice for e: "))
-    gcd, x, y = extended_euclidean(e, n)
+    gcd, x, _ = extended_euclidean(e, n)
     while gcd != 1:
         print("Choice of e is not coprime to phi_n!\nGCD: " + str(gcd))
         e = int(input("\nPlease enter a different choice for e: "))
-        gcd, x, y = extended_euclidean(e, n)
+        gcd, x, _ = extended_euclidean(e, n)
     return e, x % n
 
 def extended_euclidean(a, b):
+    """The extended euclidean algorithm
+
+    Recursively determines the GCD, as well as the x and y that satisfy
+    ax + by = GCD
+
+    Args:
+        a: the first number
+        b: the second number
+
+    Returns:
+        gcd: the GCD
+        x, y: the x and y that satisfy the extended linear combination
+    """
     if b > a:
         return extended_euclidean(b, a)
     if b == 0:
         return a, 0, 1
     # a - bq = r
     q = a // b
-    r = a % b
     gcd, x, y = extended_euclidean(b, a % b)
     return gcd, y - q * x, x
 
 
 def modular_exponentiation(base, power, modulus):
+    """Efficient modular exponentiation
+
+    Args:
+        base: the base
+        power: the power
+        modulus: the modulus
+
+    Returns:
+        The result of base ^ power (mod modulus)
+    """
     result = 1
     while power > 0:
         if power & 1 == 1:
--- program2/rsa.py
+++ program2/rsa.py
@@ -1,27 +1,141 @@
+# Jadon Schuler
+# Copyright 2020
+
+"""This module implements RSA, given values for p, q, e, d, and the message to
+encrypt, where p * q has at least 65 bits.
+
+ASCII text is split into blocks of 8 characters. If this cannot be done evenly,
+the message is padded with the null character ('\0'). Then, the program converts
+each block into an integer. To encrypt, this integer is raised to the power e
+modulo n. To decrypt, the integer is raised to the power d modulo n, then
+converted back to ASCII.
+"""
 
 
-import primegeneration as pg
-import secrets
+# Suppress variable name warning (my single-letter
+# variables are meant to be that way)
+# pylint: disable=C0103
+
 import sys
 
-def main(p, q, d, e):
+# Imported for fast modular exponentiation implementation
+import primegeneration as pg
+
+def encrypt(p, q, e, d, text):
+    """Main driver for implementing RSA
+
+    Args:
+        p: first prime
+        q: second prime
+        e: encryption exponent
+        d: decryption exponent
+    """
     n = p * q
-    y = pg.modular_exponentiation(3, e, n)
-    z = pg.modular_exponentiation(y, d, n)
-    print(z)
+
+    # Number of characters to pad with
+    padding = 8 - (len(text) % 8)
+    for i in range(padding):
+        text += '\0'
+
+    print("\n\nText to encrypt:\n" + text)
+
+    print('\n\n\n......... Encryption .........')
+    numblocks = int(len(text) / 8)
+    e_message = []
+    for i in range(numblocks):
+        blocktext = text[i*8:(i + 1) * 8]
+        print("\n\nBlock " + str(i) + ": " + blocktext)
+        e_message.append(encrypt_block(blocktext, e, n))
+        print("Encrypted ascii: " + integer_to_ascii(e_message[i]) + '\n')
+
+    # Decryption
+    print('\n\n\n......... Decryption .........')
+    d_message = ''
+    for i, block in enumerate(e_message):
+        print("\n\nBlock " + str(i) + ": " + str(block))
+        d_message += decrypt_block(block, d, n)
+
+    print("\n\nDecrypted text:\n" + d_message + '\n')
+
+def encrypt_block(blocktext, e, n):
+    """Encrypts a block using RSA
+
+    Args:
+        blocktext: Block of 8 ASCII characters to encrypt
+        e: encryption exponent
+        n: modulus
+
+    Returns:
+        Encrypted block in integer representation
+    """
+    integer = ascii_to_integer(blocktext)
+    encryption = pg.modular_exponentiation(integer, e, n)
+    print("Encrypted integer:")
+    print(str(integer) + " -> " + str(encryption))
+    return encryption
+
+def decrypt_block(encryption, d, n):
+    """Decrypts a block using RSA
+
+    Args:
+        encryption: The encrypted integer representing a block of 8 ASCII chars
+        d: decryption exponent
+        n: modulus
+
+    Retuns:
+        The decrypted block in ASCII representation
+    """
+    decryption = pg.modular_exponentiation(encryption, d, n)
+    print("Decrypted integer:")
+    print(str(encryption) + " -> " + str(decryption))
+    return integer_to_ascii(decryption)
+
+def integer_to_ascii(integer):
+    """Converts an integer into ASCII text
+
+    Args:
+        integer: integer representation of an 8 byte block of ASCII text
+
+    Returns:
+        The block of text represented by integer
+    """
+    string = ''
+    print("\nTranslating letters...")
+    for _ in range(8):
+        letter = integer % 256
+        integer = integer >> 8
+        print("{0:08b}".format(letter) + " -> " + chr(letter))
+        string = chr(letter) + string
+    return string
+
+def ascii_to_integer(text):
+    """Converts a block of ASCII text into an integer
+
+    Args:
+        text: 8 character block of ASCII text
+
+    Returns:
+        Integer representation of the ASCII text
+    """
+    integer = 0
+    for c in text:
+        integer *= 256
+        integer += ord(c)
+    return integer
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Please name an input file!")
-        exit()
+        sys.exit()
     if len(sys.argv) > 2:
         print("Too many arguments specified!")
-        exit()
-    input = open(sys.argv[1])
-    args = input.readline().split()
-    input.close()
-    p = int(args[0])
-    q = int(args[1])
-    e = int(args[2])
-    d = int(args[3])
-    main(p, q, d, e)
+        sys.exit()
+    input_file = open(sys.argv[1])
+    args = input_file.readline().split()
+    input_file.close()
+    prime_one = int(args[0])
+    prime_two = int(args[1])
+    encryption_exp = int(args[2])
+    decryption_exp = int(args[3])
+    message = 'This is the message I would like to encrypt!'
+    encrypt(prime_one, prime_two, encryption_exp, decryption_exp, message)
